a7698a8954fea6ed18dd69260475ebc4781d8222
mighty74 <103374849+mighty74@users.noreply.github.com>
2022-05-23 17:41:45 +0900

    Update settings.py

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -23,6 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '******'
 
+# 上手くいかなかった
+#try:
+#    from .local_settings import *
+#except ImportError:
+#    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
